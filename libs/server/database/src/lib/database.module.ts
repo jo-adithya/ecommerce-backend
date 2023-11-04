37bea94e8643ba3c0d44e276e035e9faf6364af2
Jonathan Adithya <jo.adithya391@gmail.com>
2023-11-04 16:10:27 -0700

    feat: update common database module

--- libs/server/database/src/lib/database.module.ts
+++ libs/server/database/src/lib/database.module.ts
@@ -1,7 +1,18 @@
 import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nx-micro-ecomm/server/config";
 
 @Module({
-	controllers: [],
+	imports: [
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				uri: configService.get("MONGODB_URI"),
+			}),
+			inject: [ConfigService],
+		}),
+	],
 	providers: [],
 	exports: [],
 })
