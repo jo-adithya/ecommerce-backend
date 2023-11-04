e6aa62629af1452fa386d9a295c37d210f37aee8
Jonathan Adithya <jo.adithya391@gmail.com>
2023-11-04 15:42:27 -0700

    feat: update common config module

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 
@@ -14,9 +9,7 @@ async function bootstrap() {
 	app.setGlobalPrefix(globalPrefix);
 	const port = process.env.PORT || 3000;
 	await app.listen(port);
-	Logger.log(
-		`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-	);
+	Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
--- libs/server/config/src/lib/config.module.ts
+++ libs/server/config/src/lib/config.module.ts
@@ -1,8 +1,17 @@
+import * as Joi from "joi";
+
 import { Module } from "@nestjs/common";
+import { ConfigModule as NestConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-	controllers: [],
-	providers: [],
-	exports: [],
+	imports: [
+		NestConfigModule.forRoot({
+			validationSchema: Joi.object({
+				MONGODB_URI: Joi.string().required(),
+			}),
+		}),
+	],
+	providers: [ConfigService],
+	exports: [ConfigService],
 })
 export class ConfigModule {}
