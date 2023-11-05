538042d912caefa5aca9e0686919d77f4c31f5d0
Jonathan Adithya <jo.adithya391@gmail.com>
2023-11-04 19:21:32 -0700

    feat: create a new users repository

---
+++ apps/auth/src/users/users.repository.ts
@@ -0,0 +1,16 @@
+import { Model } from "mongoose";
+
+import { Logger } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+
+import { AbstractRepository } from "@nx-micro-ecomm/server/database";
+
+import { User } from "../models/user.schema";
+
+export class UsersRepository extends AbstractRepository<User> {
+	protected readonly logger = new Logger(UsersRepository.name);
+
+	constructor(@InjectModel(User.name) userModel: Model<User>) {
+		super(userModel);
+	}
+}
