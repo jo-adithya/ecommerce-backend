3dd061467de7a7e691b4639690dd29e9b4559fdc
Jonathan Adithya <jo.adithya391@gmail.com>
2023-11-04 17:39:00 -0700

    feat: create product dtos with validators

---
+++ apps/products/src/dtos/create-product.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsNumber, IsPositive, IsString } from "class-validator";
+
+export class CreateProductDto {
+	@IsString()
+	@IsNotEmpty()
+	title: string;
+
+	@IsNumber()
+	@IsPositive()
+	price: number;
+}
