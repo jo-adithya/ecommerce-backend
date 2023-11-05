1d3b01246997c73f7b24c19b3d56854c65423d07
Jonathan Adithya <jo.adithya391@gmail.com>
2023-11-04 19:23:30 -0700

    feat: create a new schema and dto for users

---
+++ apps/auth/src/models/user.schema.ts
@@ -0,0 +1,14 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+import { AbstractDocument } from "@nx-micro-ecomm/server/database";
+
+@Schema({ versionKey: false, timestamps: true })
+export class User extends AbstractDocument {
+	@Prop({ required: true, unique: true })
+	email: string;
+
+	@Prop({ required: true })
+	password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
