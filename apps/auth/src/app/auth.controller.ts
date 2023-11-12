957b0808a7f97f369b57fea39695cbde8f426765
Jonathan Adithya <jo.adithya391@gmail.com>
2023-11-11 19:13:29 -0800

    feat: use the common serializer to serialize user outgoing response

--- apps/auth/src/app/auth.controller.ts
+++ apps/auth/src/app/auth.controller.ts
@@ -2,13 +2,16 @@ import { Response } from "express";
 
 import { Body, Controller, Get, Post, Res, UseGuards } from "@nestjs/common";
 
+import { Serialize } from "@nx-micro-ecomm/server/interceptors";
+
 import { CurrentUser } from "../decorators";
-import { CreateUserDto } from "../dtos";
+import { CreateUserDto, UserDto } from "../dtos";
 import { LocalAuthGuard } from "../guards/local-auth.guard";
 import { User } from "../models/user.schema";
 import { AuthService } from "./auth.service";
 
 @Controller("auth")
+@Serialize(UserDto)
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
---
+++ apps/auth/src/dtos/user.dto.ts
@@ -0,0 +1,9 @@
+import { Expose } from "class-transformer";
+
+export class UserDto {
+	@Expose({ name: "_id" })
+	id: string;
+
+	@Expose()
+	email: string;
+}
