9831edf63bfe10451de4fba8a53ba4500c9255c2
Jonathan Adithya <jo.adithya391@gmail.com>
2023-11-11 19:13:01 -0800

    feat: create a new common serializer using nest interceptor

---
+++ libs/server/interceptors/src/lib/serialize.interceptor.ts
@@ -0,0 +1,32 @@
+import { plainToInstance } from "class-transformer";
+import { Observable, map } from "rxjs";
+
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from "@nestjs/common";
+
+interface ClassConstructor {
+	new (...args: any[]): object;
+}
+
+export const Serialize = (dto: ClassConstructor) => {
+	return UseInterceptors(new SerializeInterceptor(dto));
+};
+
+class SerializeInterceptor implements NestInterceptor {
+	constructor(private readonly dto: ClassConstructor) {}
+
+	intercept(
+		context: ExecutionContext,
+		next: CallHandler<any>,
+	): Observable<any> | Promise<Observable<any>> {
+		// Intercept incoming requests
+
+		// Intercept outgoing responses
+		return next.handle().pipe(
+			map((data) => {
+				return plainToInstance(this.dto, data, {
+					excludeExtraneousValues: true,
+				});
+			}),
+		);
+	}
+}
