49908bda8ae48f6b9e051527d365272f85cdbee6
Jonathan Adithya <jo.adithya391@gmail.com>
2023-12-24 23:16:57 -0800

    feat: remove users controller (not used)

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -3,13 +3,11 @@ import { Module } from "@nestjs/common";
 import { DatabaseModule } from "@nx-micro-ecomm/server/database";
 
 import { User, UserSchema } from "../models/user.schema";
-import { UsersController } from "./users.controller";
 import { UsersRepository } from "./users.repository";
 import { UsersService } from "./users.service";
 
 @Module({
   imports: [DatabaseModule, DatabaseModule.forFeature([{ name: User.name, schema: UserSchema }])],
-  controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService],
 })
